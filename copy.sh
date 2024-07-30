5e515c3d8429ef1eb1cd0290b0069f492a280996
Alex Gartner <alexg@zetachain.com>
2024-07-30 15:01:24 -0700

    Initial commit

---
+++ copy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eo pipefail
+
+target_registry=$1
+
+source_images=(
+    # localnet test images
+    "ruimarinho/bitcoin-core:22"
+    "ethereum/client-go:v1.10.26"
+
+    # localnet build images
+    "golang:1.22.5-bookworm"
+)
+
+for source_image in "${source_images[@]}"; do
+    # replaces slashes in images names with dashes
+    target_image_name=$(echo "${source_image}" | tr '/' '-')
+    crane copy "${source_image}" "${target_registry}/${target_image_name}"
+done
